d857d92d25c0eb5ffc4c0e0e434adc3a1b48449b
zcd1997 <1021230407@qq.com>
2020-04-02 17:00:28 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import fastClick from 'fastclick'
+import './assets/style/reset.css'
+import './assets/style/border.css'
 
 Vue.config.productionTip = false
+
+fastClick.attach(document.body)
 
 new Vue({
   router,
